bf3770423e6a5a6acc7280ff5d4f344fc56ef4dd
debugmanoj <manojkumarwork01@gmail.com>
2026-04-22 17:04:19 +0530

    Fix:Career_Page

--- app/components/sections/career/CareerHero.tsx
+++ app/components/sections/career/CareerHero.tsx
@@ -5,9 +5,10 @@ import HeroWrapper from "@/app/components/shared/ui/HeroWrapper/HeroWrapper";
 export default function CareerHero() {
   return (
     // <HeroWrapper>
-      <section className="relative w-full overflow-hidden mx-auto mt-[7%] ml-[2%]">
+      <section className="relative w-full overflow-hidden mx-auto py-28 md:ml-[6%] lg:ml-[2%] ">
+      
         {/* Content */}
-        <div className="relative  max-w-7xl mx-auto text-left">
+        <div className="relative  max-w-7xl mx-auto text-center md:text-left">
           {/* Subtitle */}
           <h4 className="text-primary mx-auto mb-[2%]">
             Career
